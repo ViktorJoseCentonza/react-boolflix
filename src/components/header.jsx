5f2946cfe1980a78f4ab5fd872cbe50a06e559ca
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-04-04 12:17:57 +0200

    add basic output from api

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,7 @@
 import { useMedia } from "../contexts/Context"
 export default function Header() {
     const { media } = useMedia()
+    console.log(`this log below is from the header component! `)
     console.log(media)
     return (
         <>
@@ -8,7 +9,12 @@ export default function Header() {
                 <nav>
                     <textarea name="search" id=""></textarea>
                     <button>Search</button>
-                    <div>{media}</div>
+                    {media.map((singleMedia) => (
+                        <>
+                            <h1>{singleMedia.title}</h1>
+                            <span>{singleMedia.overview}</span>
+                        </>
+                    ))}
                 </nav>
             </header>
         </>
--- src/contexts/Context.jsx
+++ src/contexts/Context.jsx
@@ -1,14 +1,25 @@
-import { createContext, useContext, useState } from "react";
-const api_key = import.meta.env.MOVIE_DB_API_KEY;
-const base_movies_api_url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${searchText}`;
+import { createContext, useContext, useState, useEffect } from "react";
+const api_key = import.meta.env.VITE_MOVIE_DB_API_KEY;
 const Context = createContext();
 
 function MediaProvider({ children }) {
+    const [media, setMedia] = useState([]);
+    const [searchText, setsearchText] = useState('ritorno')
 
-    const [media, setMedia] = useState('Hello!');
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${searchText}`)
+            .then((res) => res.json())
+            .then((data) => {
+                setMedia(data.results)
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }, [])
+
     return (
         <Context.Provider
-            value={{ media, setMedia, }}>
+            value={{ media, setMedia, searchText, setsearchText }}>
             {children}
         </Context.Provider>
     )
